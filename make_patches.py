0be354172c5ccdf09f1bdfb931af84073ed58037
Robbie Harwood <rharwood@redhat.com>
2016-11-10 14:41:52 -0500

    Some "pythonic" cleanups

--- make_patches.py
+++ make_patches.py
@@ -5,8 +5,7 @@ import os
 files = [f for f in os.listdir(".") if f.endswith(".patch")]
 files.sort() # because git auto-numbers them for us
 
-for i in xrange(len(files)):
-    newf = files[i]
+for (i, newf) in enumerate(files):
     if files[i].endswith(".patch.patch"):
         newf = files[i][:-len(".patch")]
         pass
@@ -16,13 +15,16 @@ for i in xrange(len(files)):
     os.rename(files[i], newf)
     files[i] = newf
 
+    pass
+
+for (i, newf) in enumerate(files):
     print("Patch%d: %s" % (i + 1, newf))
     pass
 
 print("")
 
 # not necessary in all configurations, but leaving it in anyway
-for i in xrange(len(files)):
+for (i, newf) in enumerate(files):
     print("%%patch%d -p1 -b .%s" % \
           (i + 1, files[i][:-len(".patch")].replace(" ", "-")))
     pass
