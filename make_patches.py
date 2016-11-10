7a9d9b1bb4320549c3e9a54aa0748517e1e2fc59
Robbie Harwood <rharwood@redhat.com>
2016-11-10 14:35:52 -0500

    Explain what this actually does a bit

--- make_patches.py
+++ make_patches.py
@@ -3,7 +3,7 @@
 import os
 
 files = [f for f in os.listdir(".") if f.endswith(".patch")]
-files.sort()
+files.sort() # because git auto-numbers them for us
 
 for i in xrange(len(files)):
     newf = files[i]
@@ -11,6 +11,7 @@ for i in xrange(len(files)):
         newf = files[i][:-len(".patch")]
         pass
 
+    # remove git's initial numbering now that order is set
     newf = newf[5:]
     os.rename(files[i], newf)
     files[i] = newf
@@ -20,6 +21,7 @@ for i in xrange(len(files)):
 
 print("")
 
+# not necessary in all configurations, but leaving it in anyway
 for i in xrange(len(files)):
     print("%%patch%d -p1 -b .%s" % \
           (i + 1, files[i][:-len(".patch")].replace(" ", "-")))
