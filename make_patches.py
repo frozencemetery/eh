c94c4b9976fc938b15209c1ae38b18a56cf02d0a
Robbie Harwood <rharwood@redhat.com>
2016-12-05 15:12:30 -0500

    [make-patches] Create and move patches

--- make_patches.py
+++ make_patches.py
@@ -1,6 +1,48 @@
 #!/usr/bin/env python2
 
 import os
+import subprocess
+import sys
+
+def run(cmd, stderr=None, fail=False):
+    # I don't want to think about globbing
+    res = ""
+    try:
+        res = subprocess.check_output(cmd, shell=True, stderr=stderr)
+        pass
+    except subprocess.CalledProcessError as e:
+        if not fail:
+            raise e
+        res = e.output
+        pass
+    return res
+
+def test(cmd, s):
+    try:
+        run(cmd, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        print(s)
+        exit(1)
+    return
+
+argv = sys.argv
+argc = len(sys.argv)
+
+if argc < 3:
+    print("Usage: %s packagedir basetag")
+    exit(1)
+
+packagedir = argv[1]
+test("ls " + packagedir + "/.git", "package repo does not exist!")
+
+basetag = argv[2]
+test("git log " + basetag + ".." + basetag, "problem with upstream repo!")
+
+print("Everything looks okay; let's go...")
+
+run("git format-patch -N %s.." % basetag)
+
+print("Patches produced correctly...")
 
 files = [f for f in os.listdir(".") if f.endswith(".patch")]
 files.sort() # because git auto-numbers them for us
@@ -36,3 +78,12 @@ for (i, newf) in enumerate(files):
     print("%%patch%d -p1 -b .%s" % \
           (i + 1, files[i][:-len(".patch")].replace(" ", "-")))
     pass
+
+print("Moving patches...")
+
+repodir = run("pwd")[:-1] # newlines
+
+os.chdir(packagedir)
+run("git rm -f *.patch", fail=True)
+run("mv " + repodir + "/*.patch .")
+run("git add *.patch")
