145040c915e06f0e66e0261e483fe035036d1cc8
Robbie Harwood <rharwood@redhat.com>
2017-09-06 12:24:05 -0400

    Refactor to have methods

--- make_patches.py
+++ make_patches.py
@@ -31,142 +31,129 @@ def test(cmd, s):
         exit(1)
     return
 
-parser = argparse.ArgumentParser(
-    description="Munge a patched git tree into an existing spec file.")
-parser.add_argument("-p", dest="prefix", default=1, type=int,
-                    help="prefix level to use with patch(1) (default: 1)")
-parser.add_argument("-t", dest="tag", default=None,
-                    help="git tag patches are based on (default: ask git)")
-parser.add_argument("-u", dest="updateonly", action="store_true",
-                    help="no new version in spec file (default: false)")
-parser.add_argument("-n", dest="newversion", default=None,
-                    help="bump version to specified version (default: don't)")
-parser.add_argument("-d", dest="packagedir", default=None,
-                    help="package repository dir (default: from branch)")
-args = parser.parse_args()
+def verify(args):
+    if args.updateonly and args.newversion:
+        print("Error: can't specify both new version and updateonly!")
+        exit(1)
 
-if args.updateonly and args.newversion:
-    print("Error: can't specify both new version and updateonly!")
-    exit(1)
-
-branch = run("git branch | grep '^\* '")[2:-1]
-if branch == "rawhide":
-    branch = "master"
-    pass
-
-if not args.packagedir:
-    top = run("git rev-parse --show-toplevel")[:-1]
-    cwd = os.getcwd().split(os.sep)
-    while cwd[-1] in ["rawhide", branch]:
-        del(cwd[-1])
-        pass
-    cwd = cwd[-1].split(".")[0]
-
-    if "rhel" in branch:
-        cwd += ".rhel"
-        pass
-    else:
-        cwd += ".fedora"
+    args.branch = run("git branch | grep '^\* '")[2:-1]
+    if args.branch == "rawhide":
+        args.branch = "master"
         pass
 
-    args.packagedir = os.sep.join([os.getenv("HOME"), cwd])
-    pass
-
-try:
-    run("ls " + args.packagedir + "/.git", stderr=subprocess.STDOUT)
-    pass
-except subprocess.CalledProcessError:
-    args.packagedir += "/" + branch
-    test("ls " + args.packagedir + "/.git", "package repo does not exist!")
-    pass
-
-test("ls " + args.packagedir + "/*.spec", "spec file not found!")
-
-basetag = args.tag
-if not basetag:
-    basetag = run("git describe --abbrev=0 --tags")[:-1]
-    pass
-
-test("git log " + basetag + ".." + basetag,
-     "problem with upstream repo (tag: %s)!" % basetag)
-
-print("Everything looks okay; let's go...")
-
-run("rm -f *.patch", fail=True, stderr=subprocess.STDOUT)
-run("git format-patch -N %s.." % basetag)
-
-print("Patches produced correctly...")
-
-incoming_patches = [f for f in os.listdir(".") if f.endswith(".patch")]
-incoming_patches.sort() # because git auto-numbers them for us
-
-for (i, newp) in enumerate(incoming_patches):
-    newp = incoming_patches[i]
-
-    # legacy, pre-me patches
-    if newp.endswith(".patch.patch"):
-        newp = newp[:-len(".patch")]
+    if not args.tag:
+        args.tag = run("git describe --abbrev=0 --tags")[:-1]
         pass
 
-    # remove git's initial numbering now that order is set
-    newp = newp[5:]
-    os.rename(incoming_patches[i], newp)
-    incoming_patches[i] = newp
+    if not args.packagedir:
+        top = run("git rev-parse --show-toplevel")[:-1]
+        cwd = os.getcwd().split(os.sep)
+        while cwd[-1] in ["rawhide", args.branch]:
+            del(cwd[-1])
+            pass
+        cwd = cwd[-1].split(".")[0]
 
-    # strip out git's version at the bottom
-    with open(newp, "r") as f:
-        d = f.read()
+        if "rhel" in args.branch:
+            cwd += ".rhel"
+            pass
+        else:
+            cwd += ".fedora"
+            pass
+
+        args.packagedir = os.sep.join([os.getenv("HOME"), cwd])
         pass
-    d = d.replace(d[d.index("\n-- \n"):], "\n")
-    with open(newp, "w") as f:
-        f.write(d)
+
+    try:
+        run("ls " + args.packagedir + "/.git", stderr=subprocess.STDOUT)
         pass
-    pass
-
-# heavy machinery
-s = Spec(glob.glob(args.packagedir + "/*.spec")[0])
-patches_old = []
-for (k, v) in s.patches:
-    if v in incoming_patches:
-        patches_old.append((k, v))
+    except subprocess.CalledProcessError:
+        args.packagedir += "/" + args.branch
+        test("ls " + args.packagedir + "/.git",
+             "package repo %s does not exist!" % args.packagedir)
         pass
-    pass
 
-nextind = 0
-patches_res = []
-while len(incoming_patches) > 0:
-    p = incoming_patches[0]
-    del(incoming_patches[0]) # I'm the best at what I doooooooo
+    test("ls " + args.packagedir + "/*.spec", "spec file not found!")
 
-    e = [(k, v) for (k, v) in patches_old if v == p]
-    if len(e) > 0: # patch carried over from old set
-        if e[0][0] < nextind: # indexing constraint violated
-            incoming_patches.insert(0, p)
-            break
+    test("git log " + args.tag + ".." + args.tag,
+         "problem with upstream repo (tag: %s)!" % args.tag)
 
-        patches_res.append(e[0])
-        nextind = e[0][0] + 1
-        continue
+    return args
 
-    # patch is new - set nextind to next smallest unused
-    if len(patches_old) != 0:
+def produce_patches(args):
+    run("rm -f *.patch", fail=True, stderr=subprocess.STDOUT)
+    run("git format-patch -N %s.." % args.tag)
+
+    incoming_patches = [f for f in os.listdir(".") if f.endswith(".patch")]
+    incoming_patches.sort() # because git auto-numbers them for us
+
+    for (i, newp) in enumerate(incoming_patches):
+        newp = incoming_patches[i]
+
+        # legacy, pre-me patches
+        if newp.endswith(".patch.patch"):
+            newp = newp[:-len(".patch")]
+            pass
+
+        # remove git's initial numbering now that order is set
+        newp = newp[5:]
+        os.rename(incoming_patches[i], newp)
+        incoming_patches[i] = newp
+
+        # strip out git's version at the bottom
+        with open(newp, "r") as f:
+            d = f.read()
+            pass
+        d = d.replace(d[d.index("\n-- \n"):], "\n")
+        with open(newp, "w") as f:
+            f.write(d)
+            pass
+        pass
+    return incoming_patches
+
+def apply_patches(s, incoming_patches):
+    patches_old = []
+    for (k, v) in s.patches:
+        if v in incoming_patches:
+            patches_old.append((k, v))
+            pass
+        pass
+
+    nextind = 0
+    patches_res = []
+    while len(incoming_patches) > 0:
+        p = incoming_patches[0]
+        del(incoming_patches[0]) # I'm the best at what I doooooooo
+
+        e = [(k, v) for (k, v) in patches_old if v == p]
+        if len(e) > 0: # patch carried over from old set
+            if e[0][0] < nextind: # indexing constraint violated
+                incoming_patches.insert(0, p)
+                break
+
+            patches_res.append(e[0])
+            nextind = e[0][0] + 1
+            continue
+
+        # patch is new - set nextind to next smallest unused
+        if len(patches_old) != 0:
+            nextind = 1 + max([k for (k, _) in patches_res])
+            pass
+        patches_res.append((nextind, p))
+        nextind += 1
+        pass
+
+    if len(incoming_patches) > 0:
+        print("Warning: Failed to preserve existing numbering!")
+
+        # Keep backporting clearly easy, but keep the common prefix
         nextind = 1 + max([k for (k, _) in patches_res])
+        patches_res += list(enumerate(incoming_patches, nextind))
         pass
-    patches_res.append((nextind, p))
-    nextind += 1
-    pass
 
-if len(incoming_patches) > 0:
-    print("Warning: Failed to preserve existing numbering!")
+    s.patches = patches_res
+    return patches_res
 
-    # Keep backporting clearly easy, but keep the common prefix
-    nextind = 1 + max([k for (k, _) in patches_res])
-    patches_res += list(enumerate(incoming_patches, nextind))
-    pass
-
-s.patches = patches_res
-
-if not args.updateonly:
+def bookkeep(s, args):
     release = re.match("Release:\s+(\d+)", s.release).group(1)
     relnum = int(release) + 1
 
@@ -189,16 +176,9 @@ if not args.updateonly:
               (d, "Robbie Harwood <rharwood@redhat.com>",
                sep, version, relnum, msg)
     s.changelog = new_log + s.changelog
-    pass
+    return
 
-patches = ""
-for (i, newf) in patches_res:
-    patches += "%%patch%d -p%d -b .%s\n" % \
-               (i, args.prefix, newf[:-len(".patch")].replace(" ", "-"))
-    pass
-patches = patches[:-1]
-
-if "%autosetup" not in s.prep:
+def handle_autosetup(s, patches):
     # TODO(rharwood) patches are assumed to occupy only a single block here
     # I may never care enough to fix this assumption
     nopatches = [l for l in s.prep.split("\n") if not l.startswith("%patch")]
@@ -208,26 +188,78 @@ if "%autosetup" not in s.prep:
         pass
     nopatches.insert(insind + 1, patches)
     s.prep = "\n".join(nopatches)
-    pass
+    return
 
-s.sync_to_file()
+def generate_patch_section(patches_res):
+    patches = ""
+    for (i, newf) in patches_res:
+        patches += "%%patch%d -p%d -b .%s\n" % \
+                   (i, args.prefix, newf[:-len(".patch")].replace(" ", "-"))
+        pass
+    patches = patches[:-1]
+    return patches
 
-print("")
+def move_patches(args):
+    repodir = os.getcwd()
 
-print("In case %autosetup detection fails:")
-print(patches)
+    os.chdir(args.packagedir)
 
-print("Moving patches...")
+    bchange = run("git checkout " + args.branch, stderr=subprocess.STDOUT)
+    if not bchange.startswith("Already on '"):
+        sys.stderr.write(bchange)
+        pass
 
-repodir = run("pwd")[:-1] # newlines
+    run("git rm -f *.patch", fail=True)
+    run("mv " + repodir + "/*.patch .")
+    run("git add *.patch")
+    return
 
-os.chdir(args.packagedir)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Munge a patched git tree into an existing spec file.")
+    parser.add_argument("-p", dest="prefix", default=1, type=int,
+                        help="prefix level to use with patch(1) (default: 1)")
+    parser.add_argument("-t", dest="tag", default=None,
+                        help="git tag to base patches on (default: ask git)")
+    parser.add_argument("-u", dest="updateonly", action="store_true",
+                        help="no new version in spec file (default: false)")
+    parser.add_argument("-n", dest="newversion", default=None,
+                        help="bump version specified (default: don't)")
+    parser.add_argument("-d", dest="packagedir", default=None,
+                        help="package repository dir (default: from branch)")
+    args = parser.parse_args()
+    args = verify(args)
+    print("Everything looks okay; let's go...")
 
-bchange = run("git checkout " + branch, stderr=subprocess.STDOUT)
-if not bchange.startswith("Already on '"):
-    sys.stderr.write(bchange)
-    pass
+    incoming_patches = produce_patches(args)
+    print("Patches produced correctly...")
 
-run("git rm -f *.patch", fail=True)
-run("mv " + repodir + "/*.patch .")
-run("git add *.patch")
+    s = Spec(glob.glob(args.packagedir + "/*.spec")[0])
+    print("Spec file parsed...")
+
+    patches_res = apply_patches(s, incoming_patches)
+    print("New patches applied...")
+
+    if not args.updateonly:
+        bookkeep(s, args)
+        print("Kept books...")
+        pass
+
+    patches = generate_patch_section(patches_res)
+    if "%autosetup" not in s.prep:
+        handle_autosetup(s, patches)
+        print("Synced autosetup data...")
+        pass
+    print("In case %autosetup detection fails:")
+    print("")
+    print(patches)
+    print("")
+
+    s.sync_to_file()
+    print("Wrote out spec file!")
+
+    print("Moving patches...")
+    move_patches(args)
+
+    print("Done!")
+    exit(0)
