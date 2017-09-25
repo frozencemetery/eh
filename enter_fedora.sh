9f5d58bb07b45980d874aaa6af0764080741404c
Robbie Harwood <rharwood@redhat.com>
2017-09-25 16:15:56 -0400

    enter_fedora.sh is ready for use by scripts
    Run arguments from enter_fedora and remove command tracing.

--- enter_fedora.sh
+++ enter_fedora.sh
@@ -1,9 +1,9 @@
-#!/bin/sh -xe
+#!/bin/sh
 
-sudo -E sh -exc '
+sudo -E sh -c "
 mount --bind /proc ${HOME}/fedora/proc
 mount --bind /sys ${HOME}/fedora/sys
 mount --bind /dev ${HOME}/fedora/dev
 
-exec chroot ${HOME}/fedora/
-'
+exec chroot ${HOME}/fedora/ $*
+"
