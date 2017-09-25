354d0121df8af424b837c50ebc4a67a275948a4e
Robbie Harwood <rharwood@redhat.com>
2017-09-25 15:51:25 -0400

    Fix fedora chroot script by giving up on cleanup

--- enter_fedora.sh
+++ enter_fedora.sh
@@ -1,16 +1,9 @@
 #!/bin/sh -xe
 
+sudo -E sh -exc '
 mount --bind /proc ${HOME}/fedora/proc
 mount --bind /sys ${HOME}/fedora/sys
 mount --bind /dev ${HOME}/fedora/dev
-mount --bind /dev/shm ${HOME}/fedora/dev/shm
-mount --bind /dev/pts ${HOME}/fedora/dev/pts
 
-chroot ${HOME}/fedora/ || true
-
-umount ${HOME}/fedora/dev/pts
-umount ${HOME}/fedora/dev/shm
-umount ${HOME}/fedora/dev
-umount ${HOME}/fedora/sys
-umount ${HOME}/fedora/proc
-
+exec chroot ${HOME}/fedora/
+'
