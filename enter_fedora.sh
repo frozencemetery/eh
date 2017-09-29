b21a55def8679e278b9073d3c4746611a3229590
Robbie Harwood <rharwood@redhat.com>
2017-09-29 14:14:57 -0400

    Restore /dev/{pts,shm} mounts

--- enter_fedora.sh
+++ enter_fedora.sh
@@ -4,6 +4,8 @@ sudo -E sh -c "
 mount --bind /proc ${HOME}/fedora/proc
 mount --bind /sys ${HOME}/fedora/sys
 mount --bind /dev ${HOME}/fedora/dev
+mount --bind /dev/pts ${HOME}/fedora/dev/pts
+mount --bind /dev/shm ${HOME}/fedora/dev/shm
 
 exec chroot ${HOME}/fedora/ $*
 "
