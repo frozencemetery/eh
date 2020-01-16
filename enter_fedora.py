c6d0fbbe1fe17d01b390ced5f99fe119a653518d
Robbie Harwood <rharwood@redhat.com>
2020-01-16 14:16:49 -0500

    Make chroot logic more directory transparentxs

--- enter_fedora.py
+++ enter_fedora.py
@@ -8,13 +8,26 @@ import subprocess
 import sys
 
 def chroot(cmd=None, user=None):
-    if not user:
-        user = os.getuid()
+    user = user if user else os.getuid()
+
+    if not cmd:
+        shell = os.getenv("SHELL", "sh")
+        cmd = f"{shell} -i"
+    if type(cmd) != str and type(cmd) != bytes:
+        cmd = " ".join(cmd)
 
     home = os.getenv("HOME")
     if not home:
         print("go home", file=sys.stderr)
         return -1
+    home = os.path.realpath(home)
+
+    # If we're in a subdir of the chroot, continue being there.  Otherwise,
+    # start at ~ inside the chroot.
+    fedora = os.path.join(home, "fedora")
+    pwd = os.getcwd()
+    pwd = pwd[len(fedora):] if fedora in pwd else ""
+    cmd = f"cd {pwd}; {cmd}"
 
     lockfile = open(f"{home}/.chrootlock", "a")
     try:
@@ -26,25 +39,18 @@ def chroot(cmd=None, user=None):
         exit(-1)
 
     setup = []
-    if not os.path.exists(f"{home}/fedora/dev/shm"):
+    if not os.path.exists(f"{fedora}/dev/shm"):
         setup = [
-            f"mount -t proc none {home}/fedora/proc",
-            f"mount --bind /sys {home}/fedora/sys",
-            f"mount --bind /dev {home}/fedora/dev",
-            f"mount --bind /dev/pts {home}/fedora/dev/pts",
-            f"mount --bind /dev/shm {home}/fedora/dev/shm",
+            f"mount -t proc none {fedora}/proc",
+            f"mount --bind /sys {fedora}/sys",
+            f"mount --bind /dev {fedora}/dev",
+            f"mount --bind /dev/pts {fedora}/dev/pts",
+            f"mount --bind /dev/shm {fedora}/dev/shm",
         ]
     fcntl.lockf(lockfile, fcntl.LOCK_UN)
 
-    if not cmd:
-        cmd = ""
-    else:
-        if type(cmd) != str and type(cmd) != bytes:
-            cmd = " ".join(cmd)
-
-        cmd = "sh -c " + shlex.quote(cmd)
-        pass
-    chroot = f"exec chroot --userspec={user}:{user} {home}/fedora/ {cmd}"
+    cmd = "sh -c " + shlex.quote(cmd)
+    chroot = f"exec chroot --userspec={user}:{user} {fedora}/ {cmd}"
 
     script = shlex.quote(";\n".join(setup + [chroot]))
     cmd = f"sudo -E sh -c {script}"
