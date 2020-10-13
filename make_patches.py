0d594de73fa459a55e4c2b4df0ae50d3867c75f1
Robbie Harwood <rharwood@redhat.com>
2020-10-13 14:02:25 -0400

    Use --autostash when pulling

--- make_patches.py
+++ make_patches.py
@@ -280,7 +280,7 @@ if __name__ == "__main__":
     # Some people think it's a great idea to abuse their ProvenPackager status
     # to touch things that are better left alone.  They could not be more
     # wrong.  I hate this.
-    args.packagerepo.git.pull()
+    args.packagerepo.git.pull("--autostash")
 
     s = Spec(glob.glob(args.packagedir + "/*.spec")[0])
     log("Spec file parsed...")
