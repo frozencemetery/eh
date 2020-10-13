255510a7c5ad041fda4de6735f6d37c23d79cd6b
Robbie Harwood <rharwood@redhat.com>
2020-10-13 13:50:55 -0400

    Default check to cwd

--- check.sh
+++ check.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-mypy --strict $@
-exec flake8 --ignore=E261,E302,E305,E731,E741 $@
+
+args="$@"
+if [ -z "$args" ]; then
+    args=.
+fi
+
+mypy --strict $args
+exec flake8 --ignore=E261,E302,E305,E731,E741 $args
